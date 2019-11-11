c84a396f022137b44ff48ef75918fbf3a7722621
Brendan Cummings <57534348+Flamestormers@users.noreply.github.com>
2019-11-11 09:59:56 -0500

    Add files via upload

--- createTable.h
+++ createTable.h
@@ -2,21 +2,36 @@
 #include <list>
 #include <iterator>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 class createTable
 {
+   private:
+  struct listcomponents
+  {
+    string first, last;
+    int phone;
+  }
+  
+  struct table
+  {
+    int hashvalue;
+    list <listcomponents> LIST;
+  }
+    
  public:
   
-  void createTable (ifstream inFile, list <int> table[365]);
+  void createTable (table values[63]);
   /*
     Call function for to make a table
   */
 
-  int createhashvalue (int month, int day, int year);
+  int createhashvalue (int phonenumber);
   /*
     Call function to make a hash value
   */
+
 
 };
