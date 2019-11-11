074e9f6300f782ba4f03138f287190bea98f8f23
Brendan Cummings <57534348+Flamestormers@users.noreply.github.com>
2019-11-11 10:02:37 -0500

    Add files via upload

---
+++ createTable.cpp
@@ -0,0 +1,58 @@
+#ifndef "creatTable.h"
+#include "createTable.h"
+
+void createTable (table values[63])
+{
+  string first, last, filename;
+  int phoneNumber, x;
+  listcomponents info;
+  ifstream inFile;
+  for(int i=0; i<63; i++)
+    {
+      values[i].hashvalue = i;
+    }
+  cout<< "What is the name of the file I'm reading from" << endl;
+  cin >> filename;
+
+  inFile.open(filename);
+  if (!inFile)
+    {
+      cerr << "Unable to open file " << filename;
+      exit(1);
+    }
+  
+  
+  while(inFile)
+    {
+      inFile >>phoneNumber>> first >> last;
+      x= createhashvalue(phoneNumber);
+
+      info.first = first;
+      info.last = last;
+      info.phone= phoneNumber;
+      
+      values[x].LIST.push_back(info);
+    }
+
+  inFile.close;
+
+  for(int i=0; i<63; i++)
+    {
+      values[i].LIST.sort();
+    }
+      
+}
+
+int createhashvalue (int phonenumber)
+{
+  int sum=0;
+  while(phonenumber>0)
+    {
+      sum+= phonenumber%10;
+      phonenumber/=10;
+    }
+  return sum;
+}
+
+
+#endif
